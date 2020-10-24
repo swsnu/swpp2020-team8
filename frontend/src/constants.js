fefdef425298efd24a78fbf1b205606ee49f0191
curious-curie <curieyoo@snu.ac.kr>
2020-10-24 19:16:41 +0900

    feat: complete hooks tutorial component

---
+++ frontend/src/constants.js
@@ -0,0 +1,12 @@
+export const mockQuestions = [
+  {
+    id: 0,
+    question: '어디서 마시는 커피를 가장 좋아하는가?'
+  },
+  {
+    id: 1,
+    question: '가장 오래 통화해본 기억은?'
+  }
+];
+
+export const mockData = [];
