c1f754f115ee71deaf906a8b6fa7d79942d7f64d
JaeWon Kim <25769209+ssm0318@users.noreply.github.com>
2020-10-29 09:27:48 +0900

    chore: add Profile model

--- backend/adoorback/account/models.py
+++ backend/adoorback/account/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, primary_key=True)
+    question_history = models.JSONField(null=True)
+    profile_image = models.FilePathField(default='/static/boo.jpg')
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(blank=True, null=True)
+
+    def update_date(self):
+        self.updated_at = timezone.now()
+        self.save()
+
