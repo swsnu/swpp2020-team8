4863345de525e5a43d1792ff421397b058c67184
JaeWon Kim <25769209+ssm0318@users.noreply.github.com>
2020-11-04 00:22:44 +0900

    chore: modify user profile_image filefield

--- backend/adoorback/account/models.py
+++ backend/adoorback/account/models.py
@@ -1,12 +1,10 @@
 from django.core.validators import int_list_validator
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.contrib.contenttypes.fields import GenericRelation
-from django.contrib.contenttypes.models import ContentType
 
 
 class User(AbstractUser):
     question_history = models.JSONField(null=True, validators=[int_list_validator(sep=',', allow_negative=True)])
-    profile_image = models.FilePathField(default='/static/boo.jpg')
+    profile_image = models.FileField(upload_to='profile_pics', default='settings.MEDIA_ROOT/profile_pics/boo.jpg')
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True)
