e2a5768ac589c31de454414d0ccefb762de96d86
JaeWon Kim <25769209+ssm0318@users.noreply.github.com>
2020-11-03 19:13:18 +0900

    refactor: change feed app model structure

--- backend/adoorback/feed/admin.py
+++ backend/adoorback/feed/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Feed
+from .models import Article, Response, Question
 
 
-admin.site.register(Feed)
+admin.site.register(Article)
+admin.site.register(Response)
+admin.site.register(Question)
