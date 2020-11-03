1e5cbad267c3da94437f8f62b137f4f9d7e33058
JaeWon Kim <25769209+ssm0318@users.noreply.github.com>
2020-11-03 19:14:34 +0900

    chore: update utils to reflect model refactoring

--- backend/adoorback/adoorback/utils/seed.py
+++ backend/adoorback/adoorback/utils/seed.py
@@ -1,15 +1,15 @@
 from faker import Faker
 import random
 from django.contrib.auth import get_user_model
-from django.contrib.contenttypes.models import ContentType
-from feed.models import Feed, Article, AdminQuestion, CustomQuestionPost, Response, Question
+from adoorback.utils.content_types import get_content_type
+from feed.models import Article, Response, Question, Post
 from comment.models import Comment
 from like.models import Like
 import logging
 import sys
 
 
-DEBUG = True
+DEBUG = False
 
 
 def set_seed(n):
@@ -35,11 +35,11 @@ def set_seed(n):
     for _ in range(n):
         user = random.choice(users)
         Article.objects.create(author=user, content=faker.catch_phrase())
-        AdminQuestion.objects.create(author=admin, content=faker.bs())
-        CustomQuestionPost.objects.create(author=user, content=faker.word())
+        Question.objects.create(author=admin, is_admin_question=True, content=faker.bs())
+        Question.objects.create(author=user, is_admin_question=False, content=faker.word())
     logging.info(f"{Article.objects.all().count()} Article(s) created!") if DEBUG else None
-    logging.info(f"{AdminQuestion.objects.all().count()} AdminQuestion(s) created!") if DEBUG else None
-    logging.info(f"{CustomQuestionPost.objects.all().count()} CustomQuestionPost(s) created!") if DEBUG else None
+    logging.info(f"{Question.objects.all().filter(is_admin_question=True).count()} Admin Question(s) created!") if DEBUG else None
+    logging.info(f"{Question.objects.all().filter(is_admin_question=False).count()} Custom Question(s) created!") if DEBUG else None
 
     # Seed Response
     questions = Question.objects.all()
@@ -49,19 +49,21 @@ def set_seed(n):
     logging.info(f"{Response.objects.all().count()} Response(s) created!") if DEBUG else None
 
     # Seed Comment (target=Feed)
-    posts = Feed.objects.all().not_instance_of(Question)
+    articles = Article.objects.all()
     questions = Question.objects.all()
-
+    responses = Response.objects.all()
     for _ in range(n):
         user = random.choice(users)
-        post = random.choice(posts)
+        article = random.choice(articles)
         question = random.choice(questions)
-        Comment.objects.create(author=user, target=post, content=faker.catch_phrase(), is_private=_ % 2)
+        response = random.choice(responses)
+        Comment.objects.create(author=user, target=article, content=faker.catch_phrase(), is_private=_ % 2)
         Comment.objects.create(author=user, target=question, content=faker.catch_phrase(), is_private=_ % 2)
+        Comment.objects.create(author=user, target=response, content=faker.catch_phrase(), is_private=_ % 2)
     logging.info(f"{Comment.objects.all().count()} Comment(s) created!") if DEBUG else None
 
     # Seed Reply Comment (target=Comment)
-    comment_model = ContentType.objects.get_for_model(Comment)
+    comment_model = get_content_type("comment")
     comments = Comment.objects.all()
     for _ in range(n):
         user = random.choice(users)
@@ -70,16 +72,42 @@ def set_seed(n):
     logging.info(f"{Comment.objects.all().filter(content_type=comment_model).count()} Repl(ies) created!") if DEBUG else None
 
     # Seed Like
-    feed_model = ContentType.objects.get_for_model(Feed)
-    replies = Comment.objects.all().filter(content_type=comment_model)
+    replies = Comment.objects.replies_only()
     for _ in range(n):
         user = random.choice(users)
-        post = random.choice(posts)
+        article = random.choice(articles)
         question = random.choice(questions)
+        response = random.choice(responses)
         comment = random.choice(comments)
         reply = random.choice(replies)
-        Like.objects.create(user=user, target=post)
+        Like.objects.create(user=user, target=article)
         Like.objects.create(user=user, target=question)
+        Like.objects.create(user=user, target=response)
         Like.objects.create(user=user, target=comment)
         Like.objects.create(user=user, target=reply)
     logging.info(f"{Like.objects.all().count()} Like(s) created!") if DEBUG else None
+
+
+def fill_data():
+    User = get_user_model()
+    faker = Faker()
+
+    # Fill Empty Seed Data
+    questions = Question.objects.all()
+    comments = Comment.objects.all()
+    posts = Post.objects.all()
+    for user in User.objects.all():
+        Article.objects.create(author=user, content=faker.catch_phrase()) \
+            if user.article_set.all().count() == 0 else None
+        Question.objects.create(author=user, content=faker.catch_phrase(), is_admin_question=False) \
+            if user.question_set.all().count() == 0 else None
+        Response.objects.create(author=user, content=faker.catch_phrase(), target=random.choice(questions)) \
+            if user.response_set.all().count() == 0 else None
+        Comment.objects.create(author=user, content=faker.catch_phrase(), target=random.choice(posts)) \
+            if Comment.objects.comments_only().filter(author=user).count() == 0 else None
+        Comment.objects.create(author=user, content=faker.catch_phrase(), target=random.choice(comments)) \
+            if Comment.objects.replies_only().filter(author=user).count() == 0 else None
+        Like.objects.create(user=user, target=random.choice(posts)) \
+            if Like.objects.feed_likes_only().filter(user=user).count() == 0 else None
+        Like.objects.create(user=user, target=random.choice(comments)) \
+            if Like.objects.comment_likes_only().filter(user=user).count() == 0 else None
