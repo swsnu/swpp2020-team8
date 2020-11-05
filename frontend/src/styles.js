d8d291fe59cf361ee3bc71f514f01ecfcdbbe1a1
curious-curie <curieyoo@snu.ac.kr>
2020-11-05 14:59:16 +0900

    style: inject global font

--- frontend/src/styles.js
+++ frontend/src/styles.js
@@ -4,8 +4,10 @@
 import styled, { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap');
   body {
     margin: 0;
+    font-family: 'Noto Sans KR', sans-serif;
   }
 
   h1 {
