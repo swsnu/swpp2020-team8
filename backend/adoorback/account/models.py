8bd3204c8ef983c5087a25a3385d442dccb09fec
JaeWon Kim <25769209+ssm0318@users.noreply.github.com>
2020-11-05 16:06:58 +0900

    chore: modify test_coverage and user profile pic field

--- backend/adoorback/account/models.py
+++ backend/adoorback/account/models.py
@@ -5,6 +5,6 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     question_history = models.JSONField(null=True, validators=[int_list_validator(sep=',', allow_negative=True)])
-    profile_image = models.FilePathField(default='/static/boo.jpg')
+    profile_image = models.FileField(upload_to='profile_pics', default='settings.MEDIA_ROOT/profile_pics/boo.jpg')
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True)
