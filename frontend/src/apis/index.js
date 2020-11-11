d7dd1618c931bbf8f71e341f7505d3096b390aed
curious-curie <curieyoo@snu.ac.kr>
2020-11-11 15:53:02 +0900

    feat: update axios instance and user module

--- frontend/src/apis/index.js
+++ frontend/src/apis/index.js
@@ -1,10 +1,6 @@
 import axios from 'axios';
-// import history from '@/history';
 
-const instance = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/'
-});
-
+const instance = axios.create();
 // instance.interceptors.response.use(
 //   (response) => {
 //     return response;
--- frontend/src/modules/user.js
+++ frontend/src/modules/user.js
@@ -1,19 +1,64 @@
-import axios from 'axios';
+import axios from '../apis';
 
+export const SIGN_UP_REQUEST = 'user/SIGN_UP_REQUEST';
 export const SIGN_UP_SUCCESS = 'user/SIGN_UP_SUCCESS';
+export const SIGN_UP_FAILURE = 'user/SIGN_UP_FAILURE';
 export const LOGIN_REQUEST = 'user/LOGIN_REQUEST';
 export const LOGOUT = 'user/LOGOUT';
 export const LOGIN_SUCCESS = 'user/LOGIN_SUCCESS';
 export const LOGIN_FAILURE = 'user/LOGIN_FAILURE';
 export const REMOVE_ERROR = 'user/REMOVE_ERROR';
 
+export const UPDATE_QUESTION_SELECT = 'user/UPDATE_QUESTION_SELECT';
+
 const initialState = {
   loginError: false,
+  signUpError: {},
   user: {
     id: 0,
     username: '',
-    isLoggedIn: false
+    isLoggedIn: false,
+    questionHistory: null
   }
+};
+
+export const requestSignUp = (signUpInfo) => {
+  return async (dispatch) => {
+    dispatch({ type: SIGN_UP_REQUEST });
+    try {
+      // const { data } = await axios.post('api/auth/register', signUpInfo);
+      const data = await {
+        code: 200,
+        user: signUpInfo
+      };
+      if (+data.code === 200) {
+        dispatch({
+          type: SIGN_UP_SUCCESS,
+          user: data.user
+        });
+      } else {
+        dispatch({
+          type: SIGN_UP_FAILURE,
+          error: {}
+        });
+      }
+    } catch (error) {
+      dispatch({
+        type: SIGN_UP_FAILURE,
+        error
+      });
+    }
+  };
+};
+
+export const postSelectedQuestions = (selectedQuestions) => {
+  return async (dispatch) => {
+    // await axios.post('api/user/select-questions', selectedQuestions);
+    return dispatch({
+      type: UPDATE_QUESTION_SELECT,
+      selectedQuestions
+    });
+  };
 };
 
 export const signUp = (signUpInfo) => {
@@ -27,12 +72,12 @@ export const requestLogin = (email, password) => {
   return async (dispatch) => {
     dispatch(login());
     try {
-      const { data } = await axios.post('/api/user/login', email, password);
-      if (+data.code === 200) {
-        dispatch(loginSuccess(data.user));
-      } else {
-        dispatch(loginFailure(data.loginError));
-      }
+      const { data } = await axios.post('api/auth/login/', email, password);
+      // if (+data.code === 200) {
+      dispatch(loginSuccess(data.user));
+      // } else {
+      // dispatch(loginFailure(data.loginError));
+      // }
     } catch (error) {
       dispatch(loginFailure(error));
     }
@@ -107,6 +152,27 @@ export default function userReducer(state = initialState, action) {
         user: null,
         loginError: false
       };
+    case SIGN_UP_SUCCESS:
+      return {
+        ...state,
+        user: action.user,
+        signUpError: false
+      };
+    case SIGN_UP_FAILURE:
+      return {
+        ...state,
+        signUpError: action.error
+      };
+    case UPDATE_QUESTION_SELECT: {
+      const newUser = {
+        ...state.user,
+        questionHistory: action.selectedQuestions
+      };
+      return {
+        ...state,
+        user: newUser
+      };
+    }
     default:
       return state;
   }
