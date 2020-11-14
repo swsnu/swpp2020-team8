70221236faa95e94feef7f5f4c0555e1881aee97
GooJinSun <gjinsun96@gmail.com>
2020-11-14 15:26:25 +0900

    fix: <ShareSettings/> test

--- frontend/src/components/ShareSettings.test.jsx
+++ frontend/src/components/ShareSettings.test.jsx
@@ -1,35 +1,66 @@
 import { mount } from 'enzyme';
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 import { Router } from 'react-router-dom';
+import { act } from 'react-dom/test-utils';
 import history from '../history';
+import rootReducer from '../modules';
 import ShareSettings from './ShareSettings';
 
 describe('<ShareSettings />', () => {
+  const mockStore = {
+    friendReducer: {},
+    notiReducer: {},
+    postReducer: {},
+    questionReducer: {},
+    userReducer: {},
+    loadingReducer: {}
+  };
+
+  const store = createStore(
+    rootReducer,
+    mockStore,
+    composeWithDevTools(applyMiddleware(thunk))
+  );
+
   const getWrapper = () =>
     mount(
-      <Router history={history}>
-        <ShareSettings />
-      </Router>
+      <Provider store={store}>
+        <Router history={history}>
+          <ShareSettings newPost={{ content: '', type: 'Article' }} />
+        </Router>
+      </Provider>
     );
 
-  it('should render without errors', () => {
+  jest.mock('react-redux', () => ({
+    useDisPatch: () => jest.fn()
+  }));
+
+  it('should render without errors', async () => {
     const wrapper = getWrapper();
     expect(wrapper.find('ShareSettings').length).toBe(1);
   });
 
-  it('should handles with toggles', () => {
-    const wrapper = shallow(<ShareSettings />);
-    const shareWithFriendsToggle = wrapper.find('.share-with-friends');
+  it('should handles with toggles', async () => {
+    const wrapper = getWrapper();
+    const shareWithFriendsToggle = wrapper.find('.share-with-friends').at(0);
     expect(shareWithFriendsToggle.length).toBe(1);
-    shallow(shareWithFriendsToggle.prop('control')).simulate('change', {
-      target: { checked: true }
+    await act(async () => {
+      shallow(shareWithFriendsToggle.prop('control')).simulate('change', {
+        target: { checked: true }
+      });
     });
   });
 
-  it('should handles with submit button', () => {
-    const wrapper = shallow(<ShareSettings />);
-    const submitButton = wrapper.find('#submit-button');
+  it('should handles with submit button', async () => {
+    const wrapper = getWrapper();
+    const submitButton = wrapper.find('button');
     expect(submitButton.length).toBe(1);
-    submitButton.simulate('click');
+    await act(async () => {
+      submitButton.simulate('click');
+    });
   });
 });
