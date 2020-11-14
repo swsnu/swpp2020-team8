c97b80be63ff32c46a07f2034a8ac220522da984
curious-curie <curieyoo@snu.ac.kr>
2020-11-14 13:30:19 +0900

    test: fix header test

--- frontend/src/components/Header.test.jsx
+++ frontend/src/components/Header.test.jsx
@@ -1,19 +1,98 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import { mockStore } from '../mockStore';
+import rootReducer from '../modules';
+import 'jest-styled-components';
+import history from '../history';
 import Header from './Header';
 import NotificationDropdownList from './NotificationDropdownList';
 
-describe('<Header/>', () => {
-  it('should render without errors', () => {
-    const component = shallow(<Header />);
-    expect(component.length).toBe(1);
+jest.mock('../components/CustomQuestionModal', () => {
+  return jest.fn(() => {
+    return <div className="custom-question-modal" />;
+  });
+});
+
+describe('<QuestionListWidget/>', () => {
+  const store = createStore(
+    rootReducer,
+    mockStore,
+    composeWithDevTools(applyMiddleware(thunk))
+  );
+
+  const getWrapper = () =>
+    mount(
+      <Provider store={store}>
+        <Router history={history}>
+          <Header />
+        </Router>
+      </Provider>
+    );
+
+  it('should mount Question List Widget', async () => {
+    jest.mock('react-redux', () => ({
+      useDisPatch: () => jest.fn()
+    }));
+    const wrapper = getWrapper();
+    const header = wrapper.find('Header');
+    expect(header.length).toBe(1);
   });
 
   it('should render <NotificationDropdownList/> when clicking notification button', () => {
     // eslint-disable-next-line react/jsx-boolean-value
-    const component = shallow(<Header signedIn={true} />);
+    jest.mock('react-redux', () => ({
+      useDisPatch: () => jest.fn()
+    }));
+    const component = getWrapper();
     const notiButton = component.find('.noti-button');
-    notiButton.simulate('click', { stopPropagation: () => undefined });
+    notiButton.at(0).simulate('click', { stopPropagation: () => undefined });
     expect(component.find(NotificationDropdownList)).toHaveLength(1);
   });
+
+  // it('should render without errors', () => {
+  //   const component = getWrapper().find('QuestionListWidget');
+  //   expect(component.length).toBe(1);
+  // });
+
+  // it('should handle fold and unfold button clicks', () => {
+  //   const component = getWrapper();
+  //   const unfoldedButtons = component.find('button');
+
+  //   // Question: material ui를 id, class로 find하기 =>같은 class를 모두 잡아버림...
+  //   // fold
+  //   const foldButton = unfoldedButtons.at(1);
+  //   foldButton.simulate('click');
+  //   const foldedButtons = component.find('button');
+  //   expect(foldedButtons.length).toEqual(2); // unfold-button, custom-question-button
+
+  //   // unfold
+  //   const unfoldButton = foldedButtons.at(0);
+  //   unfoldButton.simulate('click');
+  //   expect(component.find('button').length).toEqual(3); // refresh, fold-button, custom-question-button
+  // });
+
+  // it('should call refresh button click', () => {
+  //   const component = getWrapper();
+  //   const questionListWidget = component.find('QuestionListWidget');
+  //   expect(questionListWidget.length).toBe(1);
+  //   const mockfn = jest.fn();
+  //   questionListWidget.handleClickRefreshButton = mockfn;
+  //   const refreshButton = component.find('button').at(0);
+  //   // expect(refreshButton.length).toBe(3);
+  //   refreshButton.simulate('click');
+  //   expect(mockfn).toHaveBeenCalledTimes(0);
+  // });
+
+  // it('should open custom question modal', () => {
+  //   const component = getWrapper();
+  //   const questionButton = component.find('CommonButton');
+  //   expect(questionButton.length).toBe(1);
+  //   questionButton.simulate('click');
+  //   expect(component.find('.custom-question-modal').length).toBe(1);
+  // });
 });
