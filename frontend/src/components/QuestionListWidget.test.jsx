d24bb07d8dfc1edac95c7463f808e13d4d78072c
GooJinSun <gjinsun96@gmail.com>
2020-11-16 16:56:11 +0900

    refactor: <QuestionListWidget/> test

--- frontend/src/components/QuestionListWidget.test.jsx
+++ frontend/src/components/QuestionListWidget.test.jsx
@@ -32,89 +32,83 @@ describe('<QuestionListWidget/>', () => {
     composeWithDevTools(applyMiddleware(thunk))
   );
 
-  const getWrapper = () =>
-    mount(
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(
       <Provider store={store}>
         <Router history={history}>
           <QuestionListWidget initialIsFolded={false} />
         </Router>
       </Provider>
     );
+  });
 
   it('should mount Question List Widget', async () => {
     jest.mock('react-redux', () => ({
       useDisPatch: () => jest.fn()
     }));
-    const wrapper = getWrapper();
     const postList = wrapper.find('ListItemLink');
     expect(postList.length).toBe(5);
   });
 
   it('should render without errors', () => {
-    const component = getWrapper().find('QuestionListWidget');
+    const component = wrapper.find('QuestionListWidget');
     expect(component.length).toBe(1);
   });
 
   it('should handle fold and unfold button clicks', () => {
-    const component = getWrapper();
-
     // fold
-    const foldButton = component.find('ExpandLessIcon').closest('button').at(0);
+    const foldButton = wrapper.find('ExpandLessIcon').closest('button').at(0);
     foldButton.simulate('click');
-    const foldedButtons = component.find('button');
+    const foldedButtons = wrapper.find('button');
     expect(foldedButtons.length).toEqual(2); // unfold-button, custom-question-button
 
     // unfold
-    const unfoldButton = component
-      .find('ExpandMoreIcon')
-      .closest('button')
-      .at(0);
+    const unfoldButton = wrapper.find('ExpandMoreIcon').closest('button').at(0);
     unfoldButton.simulate('click');
-    expect(component.find('button').length).toEqual(3); // refresh, fold-button, custom-question-button
+    expect(wrapper.find('button').length).toEqual(3); // refresh, fold-button, custom-question-button
   });
 
   it('should call refresh button click', () => {
-    const component = getWrapper();
-    const questionListWidget = component.find('QuestionListWidget');
+    const questionListWidget = wrapper.find('QuestionListWidget');
     expect(questionListWidget.length).toBe(1);
     const mockfn = jest.fn();
 
     questionListWidget.handleClickRefreshButton = mockfn;
-    const refreshButton = component.find('RefreshIcon').closest('button').at(0);
+    const refreshButton = wrapper.find('RefreshIcon').closest('button').at(0);
     refreshButton.simulate('click');
     expect(mockfn).toHaveBeenCalledTimes(0);
   });
 
   it('should open custom question modal', () => {
-    const component = getWrapper();
-    const questionButton = component.find('NewQuestionButton');
+    const questionButton = wrapper.find('NewQuestionButton');
     expect(questionButton.length).toBe(1);
     questionButton.simulate('click');
-    expect(component.find('.custom-question-modal')).toHaveLength(1);
+    expect(wrapper.find('.custom-question-modal')).toHaveLength(1);
   });
 
   it('should close custom question modal', async () => {
-    const component = getWrapper();
-    const questionButton = component.find('NewQuestionButton');
-    expect(component.find('.custom-question-modal')).toHaveLength(0);
+    const questionButton = wrapper.find('NewQuestionButton');
+    expect(wrapper.find('.custom-question-modal')).toHaveLength(0);
     expect(questionButton.length).toBe(1);
 
     await act(async () => {
       questionButton.simulate('click');
     });
-    component.update();
+    wrapper.update();
 
-    expect(component.find('.custom-question-modal')).toHaveLength(1);
+    expect(wrapper.find('.custom-question-modal')).toHaveLength(1);
 
-    const customQuestionSubmitButton = component
+    const customQuestionSubmitButton = wrapper
       .find('.custom-question-modal')
       .find('.submit-button');
 
     await act(async () => {
       customQuestionSubmitButton.simulate('click');
     });
-    component.update();
+    wrapper.update();
 
-    expect(component.find('.custom-question-modal')).toHaveLength(0);
+    expect(wrapper.find('.custom-question-modal')).toHaveLength(0);
   });
 });
