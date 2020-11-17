e9607cdce20cea40eef5ceb4758aa6365fd094d0
JaeWon Kim <25769209+ssm0318@users.noreply.github.com>
2020-11-17 17:25:33 +0900

    test: pylint 10.0

--- backend/adoorback/comment/serializers.py
+++ backend/adoorback/comment/serializers.py
@@ -3,7 +3,6 @@ from django.contrib.auth import get_user_model
 
 from comment.models import Comment
 from adoorback.serializers import AdoorBaseSerializer
-from adoorback.utils.content_types import get_content_type
 
 
 User = get_user_model()
--- backend/adoorback/comment/tests.py
+++ backend/adoorback/comment/tests.py
@@ -76,7 +76,7 @@ class CommentAPITestCase(APITestCase):
         with self.login(username=current_user.username, password='password'):
             response = self.get('comment-list')
             self.assertEqual(response.status_code, 200)
-            self.assertEqual(response.data['count'], N*2)
+            self.assertEqual(response.data['count'], N*3)
 
     def test_reply_list(self):
         current_user = self.make_user(username='current_user')
