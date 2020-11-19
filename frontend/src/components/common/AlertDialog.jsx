b110b7a07b265ab59170ad87661c8f22b3ae671d
curious-curie <curieyoo@Curiesui-MacBook-Pro.local>
2020-11-19 12:56:12 +0900

    feat: implement common dialog component

---
+++ frontend/src/components/common/AlertDialog.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+
+export default function AlertDialog({ onConfirm, onClose, message, isOpen }) {
+  return (
+    <Dialog
+      open={isOpen}
+      onClose={onClose}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          {message}
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onConfirm} color="primary" autoFocus>
+          확인
+        </Button>
+        <Button onClick={onClose} color="secondary">
+          취소
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
