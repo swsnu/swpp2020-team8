fcddeb00ac457ef4d8cd32133a8d678973bae5a4
GooJinSun <gjinsun96@gmail.com>
2020-11-24 01:01:48 +0900

    test: add question module test

--- frontend/src/modules/question.test.js
+++ frontend/src/modules/question.test.js
@@ -86,4 +86,44 @@ describe('questionActions', () => {
       );
     });
   });
+
+  it(`'getFriendResponsesByQuestion' should get responses of selected question correctly`, (done) => {
+    jest.mock('axios');
+
+    const spy = jest.spyOn(axios, 'get').mockImplementation(() => {
+      return new Promise((resolve) => {
+        const res = {
+          data: questionDetailPosts
+        };
+        resolve(res);
+      });
+    });
+
+    store.dispatch(actionCreators.getFriendResponsesByQuestion()).then(() => {
+      const newState = store.getState();
+      expect(spy).toHaveBeenCalled();
+      expect(newState.questionReducer.selectedQuestion).toMatchObject(
+        questionDetailPosts
+      );
+      expect(newState.questionReducer.selectedQuestionResponses).toMatchObject(
+        questionDetailPosts.response_set
+      );
+      done();
+    });
+  });
+
+  it('should dispatch question/GET_SELECTED_QUESTION_RESPONSES_FAILURE when api returns error', async () => {
+    jest.mock('axios');
+    const spy = jest.spyOn(axios, 'get').mockImplementation(() => {
+      return Promise.reject(new Error('error'));
+    });
+
+    store.dispatch(actionCreators.getFriendResponsesByQuestion()).then(() => {
+      const newState = store.getState();
+      expect(spy).toHaveBeenCalled();
+      expect(newState.questionReducer.questionDetailPosts).toMatchObject(
+        questionDetailPosts
+      );
+    });
+  });
 });
