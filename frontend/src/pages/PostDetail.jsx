a1f63bc2c3c014927f2612082b4871e72d2130fb
curious-curie <curieyoo@Curiesui-MacBook-Pro.local>
2020-11-24 01:38:27 +0900

    feat: implement post-delete

--- frontend/src/pages/PostDetail.jsx
+++ frontend/src/pages/PostDetail.jsx
@@ -29,6 +29,7 @@ export default function PostDetail() {
         <PostItem
           postObj={selectedPost}
           postKey={`${selectedPost.type}-${selectedPost.id}`}
+          isDetailPage
         />
       )}
     </div>
