d94fa7d0454cef3e7c790e574d747c3d0ace01f8
Jina Park <koyrkr@gmail.com>
2020-11-26 17:35:31 +0900

    fix: changed enter keycode with key

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -47,7 +47,7 @@ export default function Login() {
   };
 
   const onKeySubmit = (e) => {
-    if (e.keyCode === 13) {
+    if (e.key === 'Enter') {
       dispatch(requestLogin(loginInfo));
     }
   };
