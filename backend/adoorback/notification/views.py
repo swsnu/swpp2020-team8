a71ffbb37310340e96b4f458030ae0e5946ddcdd
GooJinSun <gjinsun96@gmail.com>
2020-11-27 05:19:01 +0900

    feat: add read all notifications api

--- backend/adoorback/notification/views.py
+++ backend/adoorback/notification/views.py
@@ -1,5 +1,10 @@
 from rest_framework import generics
 from rest_framework import permissions
+from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework.response import Response as DRF_Response
+from django.http import HttpResponse, HttpResponseNotAllowed, JsonResponse
+from rest_framework.pagination import PageNumberPagination
 
 from notification.models import Notification
 from notification.serializers import NotificationSerializer
@@ -7,17 +12,25 @@ from notification.serializers import NotificationSerializer
 from adoorback.permissions import IsRecipient
 
 
-class NotificationList(generics.ListAPIView):
-    """
-    List all comments, or create a new notification.
-    """
-    serializer_class = NotificationSerializer
-    permission_classes = [permissions.IsAuthenticated]
+@api_view(["GET", "PUT"])
+def notification_list(request):
+    if not request.user.is_authenticated:
+        return HttpResponse(status=401)
 
-    def get_queryset(self):
-        queryset = Notification.objects.filter(recipient_id = self.request.user.id).order_by('-created_at')
-        return queryset
+    notifications = Notification.objects.filter(recipient_id=request.user.id, is_visible=True).order_by('-created_at')
+    paginator = PageNumberPagination()
+    paginator.page_size = 15
 
+    if request.method == 'GET':
+        paginated_result = paginator.paginate_queryset(notifications, request)
+        serializer = NotificationSerializer(paginated_result, many=True, context={'request': request});
+        return paginator.get_paginated_response(serializer.data)
+
+    elif request.method == 'PUT':
+        notifications.update(is_read=True);
+        paginated_result = paginator.paginate_queryset(notifications, request)
+        serializer = NotificationSerializer(paginated_result, many=True, context={'request': request});
+        return paginator.get_paginated_response(serializer.data)
 
 class NotificationDetail(generics.UpdateAPIView):
     queryset = Notification.objects.all()
