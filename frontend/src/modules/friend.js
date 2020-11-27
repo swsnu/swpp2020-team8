7c5019f1837925fb4ba75d2ebb618d6d8346aec7
GooJinSun <gjinsun96@gmail.com>
2020-11-28 00:12:40 +0900

    merge: with develop

--- frontend/src/modules/friend.js
+++ frontend/src/modules/friend.js
@@ -1,21 +1,34 @@
 import axios from '../apis';
-// import { mockFriendList } from '../constants';
 
 const initialState = {
   friendList: [],
   selectedUser: {},
-  selectedUserPosts: []
+  selectedUserPosts: [],
+  friendRequestError: false
 };
 
 export const GET_FRIEND_LIST_REQUEST = 'friend/GET_FRIEND_LIST_REQUEST';
 export const GET_FRIEND_LIST_SUCCESS = 'friend/GET_FRIEND_LIST_SUCCESS';
 export const GET_FRIEND_LIST_FAILURE = 'friend/GET_FRIEND_LIST_FAILURE';
 
+export const REQUEST_FRIEND_REQUEST = 'friend/REQUEST_FRIEND_REQUEST';
+export const REQUEST_FRIEND_SUCCESS = 'friend/REQUEST_FRIEND_SUCCESS';
+export const REQUEST_FRIEND_FAILURE = 'friend/REQUEST_FRIEND_FAILURE';
+
 export const DELETE_FRIEND_REQUEST = 'friend/DELETE_FRIEND_REQUEST';
 export const DELETE_FRIEND_SUCCESS = 'friend/DELETE_FRIEND_SUCCESS';
 export const DELETE_FRIEND_FAILURE = 'friend/DELETE_FRIEND_FAILURE';
 
-export const ADD_TO_FRIEND_LIST = 'friend/ADD_TO_FRIEND_LIST';
+export const DELETE_REQUEST_REQUEST = 'friend/DELETE_REQUEST_REQUEST';
+export const DELETE_REQUEST_SUCCESS = 'friend/DELETE_REQUEST_SUCCESS';
+export const DELETE_REQUEST_FAILURE = 'friend/DELETE_REQUEST_FAILURE';
+
+export const ACCEPT_FRIEND_REQUEST_REQUEST =
+  'friend/ACCEPT_FRIEND_REQUEST_REQUEST';
+export const ACCEPT_FRIEND_REQUEST_SUCCESS =
+  'friend/ACCEPT_FRIEND_REQUEST_SUCCESS';
+export const ACCEPT_FRIEND_REQUEST_FAILURE =
+  'friend/ACCEPT_FRIEND_REQUEST_FAILURE';
 
 export const getFriendList = () => async (dispatch) => {
   dispatch({ type: GET_FRIEND_LIST_REQUEST });
@@ -24,6 +37,7 @@ export const getFriendList = () => async (dispatch) => {
     result = await axios.get(`user/friends/`);
   } catch (err) {
     dispatch({ type: GET_FRIEND_LIST_FAILURE, error: err });
+    return;
   }
   dispatch({
     type: GET_FRIEND_LIST_SUCCESS,
@@ -35,9 +49,10 @@ export const deleteFriend = (friendId) => async (dispatch, getState) => {
   const userId = getState().userReducer.user?.id;
   dispatch({ type: DELETE_FRIEND_REQUEST, userId });
   try {
-    // await axios.delete(`/user/friendship/${friendId}/`);
+    await axios.delete(`user/friendship/${friendId}/`);
   } catch (err) {
     dispatch({ type: DELETE_FRIEND_FAILURE, error: err });
+    return;
   }
   dispatch({
     type: DELETE_FRIEND_SUCCESS,
@@ -45,19 +60,44 @@ export const deleteFriend = (friendId) => async (dispatch, getState) => {
   });
 };
 
-export const acceptFriendRequest = (friendObj) => {
-  // need api actions
-  return {
-    type: ADD_TO_FRIEND_LIST,
-    friend: friendObj
-  };
+export const deleteFriendRequest = (requestId) => async (dispatch) => {
+  dispatch({ type: DELETE_REQUEST_REQUEST });
+  try {
+    await axios.delete(`user/friend-requests/${requestId}/`);
+  } catch (err) {
+    dispatch({ type: DELETE_REQUEST_FAILURE, error: err });
+    return;
+  }
+  dispatch({
+    type: DELETE_REQUEST_SUCCESS,
+    requestId
+  });
 };
 
-export const addFriend = (friendObj) => {
-  return {
-    type: ADD_TO_FRIEND_LIST,
-    friend: friendObj
-  };
+export const requestFriend = (responderId) => async (dispatch, getState) => {
+  const userId = getState().userReducer.user?.id;
+  dispatch({
+    type: REQUEST_FRIEND_REQUEST
+  });
+  await axios.post('user/friend-requests/', {
+    responder_id: responderId,
+    requester_id: userId
+  });
+
+  dispatch({
+    type: REQUEST_FRIEND_SUCCESS,
+    responderId
+  });
+};
+
+export const acceptFriendRequest = (requestId) => async (dispatch) => {
+  dispatch({
+    type: ACCEPT_FRIEND_REQUEST_REQUEST
+  });
+  await axios.patch(`user/friend-requests/${requestId}/`, {
+    responded: true
+  });
+  dispatch(getFriendList());
 };
 
 export default function friendReducer(state = initialState, action) {
@@ -81,12 +121,10 @@ export default function friendReducer(state = initialState, action) {
         ...state,
         friendList: newFriendList
       };
-    case ADD_TO_FRIEND_LIST:
+    case REQUEST_FRIEND_FAILURE:
       return {
         ...state,
-        friendList: state.friendList
-          ? [...state.friendList, action.friend]
-          : [action.friend]
+        friendRequestError: action.error
       };
     default:
       return state;
--- frontend/src/modules/friend.test.js
+++ frontend/src/modules/friend.test.js
@@ -30,13 +30,93 @@ describe('friendActions', () => {
     });
   });
 
+  it(`should not modify friend list when get friend list has error`, (done) => {
+    const spy = jest.spyOn(axios, 'get').mockImplementation(() => {
+      return Promise.reject(new Error('error'));
+    });
+    store.dispatch(actionCreators.getFriendList()).then(() => {
+      const newState = store.getState();
+      expect(spy).toHaveBeenCalledTimes(1);
+      expect(newState.friendReducer.friendList).toMatchObject([]);
+      done();
+    });
+  });
+
   it(`'deleteFriend' should delete friend correctly`, (done) => {
+    const spy = jest.spyOn(axios, 'delete').mockImplementation(() => {
+      return new Promise((resolve) => {
+        resolve();
+      });
+    });
     store.dispatch(actionCreators.deleteFriend(1)).then(() => {
       const newState = store.getState();
       const { friendList } = newState.friendReducer;
-      //   expect(spy).toHaveBeenCalledTimes(1);
+      expect(spy).toHaveBeenCalledTimes(1);
       const newFriendList = friendList.filter((friend) => friend.id !== 1);
       expect(newState.friendReducer.friendList).toMatchObject(newFriendList);
+      done();
+    });
+  });
+
+  it(`should not modify friend list when delete friend has error`, (done) => {
+    const spy = jest.spyOn(axios, 'delete').mockImplementation(() => {
+      return Promise.reject(new Error('error'));
+    });
+    const { friendList } = store.getState().friendReducer;
+
+    store.dispatch(actionCreators.deleteFriend(1)).then(() => {
+      const newState = store.getState();
+      expect(spy).toHaveBeenCalledTimes(1);
+      expect(newState.friendReducer.friendList).toMatchObject(friendList);
+      done();
+    });
+  });
+
+  it(`should not modify friend list when delete friend request has error`, (done) => {
+    const spy = jest.spyOn(axios, 'delete').mockImplementation(() => {
+      return Promise.reject(new Error('error'));
+    });
+    store.dispatch(actionCreators.deleteFriendRequest(1)).then(() => {
+      const newState = store.getState();
+      const { friendList } = newState.friendReducer;
+      expect(spy).toHaveBeenCalledTimes(1);
+      expect(newState.friendReducer.friendList).toMatchObject(friendList);
+      done();
+    });
+  });
+
+  it(`'deleteFriendRequest' should make delete call`, (done) => {
+    const spy = jest.spyOn(axios, 'delete').mockImplementation(() => {
+      return new Promise((resolve) => {
+        resolve();
+      });
+    });
+    store.dispatch(actionCreators.deleteFriendRequest(1)).then(() => {
+      expect(spy).toHaveBeenCalledTimes(1);
+      done();
+    });
+  });
+
+  it(`'acceptFriendRequest' should make patch call`, (done) => {
+    const spy = jest.spyOn(axios, 'patch').mockImplementation(() => {
+      return new Promise((resolve) => {
+        resolve();
+      });
+    });
+    store.dispatch(actionCreators.acceptFriendRequest(1)).then(() => {
+      expect(spy).toHaveBeenCalledTimes(1);
+      done();
+    });
+  });
+
+  it(`'requestFriend' should make post call`, (done) => {
+    const spy = jest.spyOn(axios, 'post').mockImplementation(() => {
+      return new Promise((resolve) => {
+        resolve();
+      });
+    });
+    store.dispatch(actionCreators.requestFriend(1)).then(() => {
+      expect(spy).toHaveBeenCalledTimes(1);
       done();
     });
   });
@@ -48,7 +128,8 @@ describe('Friend Reducer', () => {
     expect(newState).toEqual({
       friendList: [],
       selectedUser: {},
-      selectedUserPosts: []
+      selectedUserPosts: [],
+      friendRequestError: false
     });
   });
 
@@ -57,7 +138,8 @@ describe('Friend Reducer', () => {
       {
         friendList: [],
         selectedUser: {},
-        selectedUserPosts: []
+        selectedUserPosts: [],
+        friendRequestError: false
       },
       {
         type: actionCreators.GET_FRIEND_LIST_SUCCESS,
@@ -67,7 +149,8 @@ describe('Friend Reducer', () => {
     expect(newState).toMatchObject({
       friendList: mockFriendList,
       selectedUser: {},
-      selectedUserPosts: []
+      selectedUserPosts: [],
+      friendRequestError: false
     });
   });
 
@@ -76,7 +159,8 @@ describe('Friend Reducer', () => {
       {
         friendList: mockFriendList,
         selectedUser: {},
-        selectedUserPosts: []
+        selectedUserPosts: [],
+        friendRequestError: false
       },
       {
         type: actionCreators.DELETE_FRIEND_SUCCESS,
@@ -89,7 +173,30 @@ describe('Friend Reducer', () => {
     expect(newState).toMatchObject({
       friendList: newFriendList,
       selectedUser: {},
-      selectedUserPosts: []
+      selectedUserPosts: [],
+      friendRequestError: false
+    });
+  });
+
+  it('should reflect error when friend request failed', () => {
+    const newState = friendReducer(
+      {
+        friendList: mockFriendList,
+        selectedUser: {},
+        selectedUserPosts: [],
+        friendRequestError: false
+      },
+      {
+        type: actionCreators.REQUEST_FRIEND_FAILURE,
+        error: 'error'
+      }
+    );
+
+    expect(newState).toMatchObject({
+      friendList: mockFriendList,
+      selectedUser: {},
+      selectedUserPosts: [],
+      friendRequestError: 'error'
     });
   });
 });
