60d2e6bcc39b0e1eff790231ca87813bfc838178
JaeWon Kim <25769209+ssm0318@users.noreply.github.com>
2020-12-03 18:48:38 +0900

    chore: add target id(type) to comment(like) serializers

--- backend/adoorback/like/serializers.py
+++ backend/adoorback/like/serializers.py
@@ -12,6 +12,8 @@ class LikeSerializer(serializers.ModelSerializer):
     user = serializers.HyperlinkedIdentityField(
         view_name='user-detail', read_only=True)
     user_detail = serializers.SerializerMethodField(read_only=True)
+    target_type = serializers.SerializerMethodField()
+    target_id = serializers.SerializerMethodField()
 
     def get_user_detail(self, obj):
         user = obj.user
@@ -20,6 +22,12 @@ class LikeSerializer(serializers.ModelSerializer):
             "username": user.username,
         }
 
+    def get_target_type(self, obj):
+        return obj.target.type
+
+    def get_target_id(self, obj):
+        return obj.object_id
+
     class Meta(AdoorBaseSerializer.Meta):
         model = Like
-        fields = ['id', 'type', 'user', 'user_detail']
+        fields = ['id', 'type', 'user', 'user_detail', 'target_type', 'target_id']
