79429a31915f2f9eb1a3bd2867596ba916e7d54a
curious-curie <curieyoo@snu.ac.kr>
2020-12-06 19:48:10 +0900

    fix: change ga

--- frontend/src/ga.js
+++ frontend/src/ga.js
@@ -1,7 +1,9 @@
 import ReactGA from 'react-ga';
 
 export const initGA = () => {
-  ReactGA.initialize('G-WYLPK7NZVE');
+  ReactGA.initialize('UA-184794346-1', {
+    debug: true
+  });
 };
 
 export const trackPage = (path) => {
