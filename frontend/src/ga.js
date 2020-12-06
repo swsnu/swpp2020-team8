d65b840937076ee3a59a3ce2cd3d2afb7f5ae9be
Curie Yoo <curieyoo@snu.ac.kr>
2020-12-06 23:42:34 +0900

    Update ga.js

--- frontend/src/ga.js
+++ frontend/src/ga.js
@@ -1,9 +1,7 @@
 import ReactGA from 'react-ga';
 
 export const initGA = () => {
-  ReactGA.initialize('UA-184794346-1', {
-    debug: true
-  });
+  ReactGA.initialize('UA-184794346-1');
 };
 
 export const trackPage = (path) => {
